7d933f6698e3f319a5611024d93975aecbbe6239
Sybren Dijkstra <nerbiz@gmail.com>
2021-11-18 13:23:15 +0100

    Changed background, simplified script

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
 var contentField = document.getElementById('content');
 var storeTimeout = null;
 
-function readValue()
-{
-    // Make sure the entry exists in localStorage
-    var currentValue = localStorage.fieldContents;
-    if (currentValue === undefined) {
-        localStorage.fieldContents = '';
-    }
-
-    return localStorage.fieldContents;
-}
-
-function storeValue(value)
-{
-    localStorage.fieldContents = value;
+// Make sure the entry exists in localStorage
+var currentValue = localStorage.fieldContents;
+if (currentValue === undefined) {
+    localStorage.fieldContents = '';
 }
 
 // Restore the previous value on pageload
-contentField.value = readValue();
+contentField.value = localStorage.fieldContents;
 
 // Store the value while typing
 contentField.addEventListener('keydown', function(event) {
     // Delay before storing the value
     clearTimeout(storeTimeout);
     storeTimeout = setTimeout(function() {
-        storeValue(contentField.value);
+        localStorage.fieldContents = contentField.value;
     }, 500);
 });
